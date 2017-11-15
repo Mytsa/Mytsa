d9fd40651868f451ff29eb3684b89da60abf227e
Viktor <31900832+Mytsa@users.noreply.github.com>
2017-11-15 23:09:46 +0200

    del

--- HW-5(check_ the_number).py
+++
@@ -1,9 +0,0 @@
-num = int(raw_input("input number: "))
-
-i = 2
-for i in range(2, num):
-    if num % i == 0:
-        print("you input NOT prime number")
-    else:
-        print("you input prime number")
-    break
