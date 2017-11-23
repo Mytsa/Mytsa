0b5ae0c8662f0f4d653006beba48ac1e7263a6c2
Mytsa <viktor.mytsa@gmail.com>
2017-11-23 23:26:18 +0200

    add commit in codean and write in correction print

--- cycles.py
+++ cycles.py
@@ -14,7 +14,7 @@ if c is 'y':
     counter = int(input('input counter data: '))
     correction = counter - cycles
     total_cycles = counter
-    print(correction)
+    print('correction is: {} '.format(correction))
 else:
     total_cycles = int(input('input total cycles: '))
 
@@ -27,7 +27,7 @@ rb = xlrd.open_workbook('apl_counter.xls', formatting_info=True)
 # choice active sheet
 sheet = rb.sheet_by_index(0)
 i = 0
-while sheet.row_values(i)[0] != '**':
+while sheet.row_values(i)[0] != '**':  # find last row before mark --> **
     x = i + 1
     i += 1
     # print(x)
@@ -42,7 +42,7 @@ add(x, 1, date)
 add(x, 2, page)
 add(x, 3, cycles)
 add(x, 4, total_cycles)
-ws.write(x + 1, 0, "**")
+ws.write(x + 1, 0, "**")  # mark last row with symbol --> **
 
 wb.save('apl_counter.xls')
 
