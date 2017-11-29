282c4b58bb8800ba6d99ac4cb076780dfd0c22cd
Mytsa <viktor.mytsa@gmail.com>
2017-11-29 20:45:11 +0200

    add window with PyQt

--- window.py
+++ window.py
@@ -1,25 +1,133 @@
-import sys
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+# -*- coding: utf-8 -*-
 
-# app = QApplication(sys.argv)
+# Form implementation generated from reading ui file 'window.ui'
 #
-# window = QWidget()
-# window.setGeometry(100, 100, 300, 300)
-# window.setWindowTitle('calculation of cycles')
+# Created by: PyQt5 UI code generator 5.6
 #
-# window.show()
-# sys.exit(app.exec())
+# WARNING! All changes made in this file will be lost!
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Window(QMainWindow):
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setGeometry(100, 100, 300, 300)
-        self.setWindowTitle('calculation of cycles')
-        # self.setWindowIcon(QGui.QIcon('namefile.png'))  # add image in window
-        self.show()
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(980, 406)
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(30, 70, 281, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_2.setGeometry(QtCore.QRect(340, 70, 61, 41))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_3 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_3.setGeometry(QtCore.QRect(30, 140, 281, 41))
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(30, 50, 101, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(340, 50, 101, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(30, 120, 101, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(30, 190, 101, 16))
+        self.label_4.setObjectName("label_4")
+        self.textEdit_4 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_4.setGeometry(QtCore.QRect(30, 210, 281, 41))
+        self.textEdit_4.setObjectName("textEdit_4")
+        self.checkBox = QtWidgets.QCheckBox(Dialog)
+        self.checkBox.setGeometry(QtCore.QRect(340, 140, 71, 41))
+        self.checkBox.setObjectName("checkBox")
+        self.textEdit_5 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_5.setGeometry(QtCore.QRect(610, 70, 261, 41))
+        self.textEdit_5.setObjectName("textEdit_5")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(610, 50, 101, 16))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(610, 120, 101, 16))
+        self.label_6.setObjectName("label_6")
+        self.textEdit_6 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_6.setGeometry(QtCore.QRect(610, 140, 261, 41))
+        self.textEdit_6.setObjectName("textEdit_6")
+        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(610, 190, 101, 16))
+        self.label_7.setObjectName("label_7")
+        self.textEdit_8 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_8.setGeometry(QtCore.QRect(610, 210, 61, 31))
+        self.textEdit_8.setObjectName("textEdit_8")
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(610, 10, 251, 21))
+        self.label_8.setObjectName("label_8")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(780, 210, 91, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.textEdit_7 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_7.setGeometry(QtCore.QRect(30, 280, 281, 41))
+        self.textEdit_7.setObjectName("textEdit_7")
+        self.label_9 = QtWidgets.QLabel(Dialog)
+        self.label_9.setGeometry(QtCore.QRect(30, 260, 101, 16))
+        self.label_9.setObjectName("label_9")
+        self.label_10 = QtWidgets.QLabel(Dialog)
+        self.label_10.setGeometry(QtCore.QRect(30, 10, 251, 21))
+        self.label_10.setObjectName("label_10")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 340, 281, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
 
-app = QApplication(sys.argv)
-Gui = Window()
-sys.exit(app.exec_())
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.textEdit.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.textEdit_2.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.textEdit_3.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label.setText(_translate("Dialog", "Applicator"))
+        self.label_2.setText(_translate("Dialog", "card page"))
+        self.label_3.setText(_translate("Dialog", "cycles in card"))
+        self.label_4.setText(_translate("Dialog", "cycles in old card"))
+        self.textEdit_4.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.checkBox.setText(_translate("Dialog", "counter"))
+        self.textEdit_5.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label_5.setText(_translate("Dialog", "total cycles"))
+        self.label_6.setText(_translate("Dialog", "correction"))
+        self.textEdit_6.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label_7.setText(_translate("Dialog", "next card page"))
+        self.label_8.setText(_translate("Dialog", "CHECK THIS DATA IN NEW CARD"))
+        self.pushButton.setText(_translate("Dialog", "PRINT"))
+        self.textEdit_7.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label_9.setText(_translate("Dialog", "counter cycles"))
+        self.label_10.setText(_translate("Dialog", "INPUT DATA FROM CARD"))
+        self.pushButton_2.setText(_translate("Dialog", "CALCULATION"))
+
