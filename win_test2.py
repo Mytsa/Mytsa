49ed8aa345e9fa600ee41e115dd6dad292748a25
Mytsa <viktor.mytsa@gmail.com>
2017-12-07 21:46:47 +0200

    correct print pages

--- win_test2.py
+++ win_test2.py
@@ -12,23 +12,31 @@ class MyWin(QtWidgets.QDialog):
         self.ui.setupUi(self)
 
         self.ui.pushButton.clicked.connect(self.action)    # print data - work
-        self.ui.pushButton_2.clicked.connect(self.print_card)    # print data - work
+        self.ui.pushButton_2.clicked.connect(self.print_card)    # print data 1 page
+        self.ui.pushButton_3.clicked.connect(self.print_card2)    # print data 2 page
 
     def action(self):
         self.ui.total_cycles.setText("")    # total_cycles
         self.ui.next_page.setText("")    # next page
         self.ui.correction.setText("")    # correction
+        self.ui.total_cycles1.setText("")    # Sum of cycles in card
 
+        # input data
         applicator = self.ui.applicator.toPlainText()
         cycles = self.ui.cycles.toPlainText()
-        old_cycles = self.ui.old_cycles.toPlainText()
+        f_cycles = self.ui.f_cycles.toPlainText()
+        initial_cycles = self.ui.initial_cycles.toPlainText()
         page = self.ui.page.toPlainText()
-
         counter = self.ui.counter.toPlainText()
-        total_cycles = str(int(cycles) + int(old_cycles))
+
+
+        total_cycles = str(int(cycles) + int(initial_cycles))
+
+        total_cycles1 = str((int(cycles) + int(initial_cycles)) - int(f_cycles))
+        self.ui.total_cycles1.setText(total_cycles1)
 
         if counter != total_cycles:
-            correction = str((int(counter) - (int(cycles) + int(old_cycles))))
+            correction = str((int(counter) - (int(cycles) + int(initial_cycles))))
             self.ui.total_cycles.setText(counter)
             self.ui.correction.setText(correction)
         else:
@@ -72,21 +80,41 @@ class MyWin(QtWidgets.QDialog):
         wrfile.save('counter_ver.1.xlsx')
 
 
-        # write data to file
-        wb = load_workbook(filename='Template_apl_cycles.xlsx', read_only=True)
+        # write data to template file, 1 page
         wrfile = load_workbook('Template_apl_cycles.xlsx')
 
-        b = str('B') + str(80)
-        c = str('C') + str(75)
+        b = str('B') + str(8)
+        i = str('I') + str(8)
+        f = str('F') + str(44)
 
-        sheet = wrfile.get_sheet_by_name('print')
-        sheet[b] = "correction is: {}".format(correction)
-        sheet[c] = int(total_cycles)
+        sheet = wrfile.get_sheet_by_name('print1')
+        sheet[b] = datetime.strftime(datetime.now(), "%Y")
+        sheet[i] = int(applicator)
+        sheet[f] = int(page1)
 
         wrfile.save('Template_apl_cycles.xlsx')
 
+
+        # write data to template file, 2 page
+        wrfile = load_workbook('Template_apl_cycles.xlsx')
+
+        d = str('D') + str(30)
+        b = str('B') + str(35)
+
+        sheet = wrfile.get_sheet_by_name('print2')
+        sheet[b] = "correction is: {}".format(correction)
+        sheet[d] = int(total_cycles) + int(correction)
+
+        wrfile.save('Template_apl_cycles2.xlsx')
+
+
+    # print 2 sheets in 2 file
     def print_card(self):
         os.startfile("I:/python/code/cycles_apl/Template_apl_cycles.xlsx", "print")    # write correct address, file name
+
+    def print_card2(self):
+        os.startfile("I:/python/code/cycles_apl/Template_apl_cycles2.xlsx", "print")  # write correct address, file name
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
