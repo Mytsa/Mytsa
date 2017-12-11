a704823195a5b1e9211c3047b9b205abfaf45949
Mytsa <viktor.mytsa@gmail.com>
2017-12-11 20:47:26 +0200

    ad work version

--- win_test2.py
+++ win_test2.py
@@ -5,15 +5,16 @@ from window2 import *
 from openpyxl import *
 from datetime import datetime
 
+
 class MyWin(QtWidgets.QDialog):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton.clicked.connect(self.action)    # print data - work
-        self.ui.pushButton_2.clicked.connect(self.print_card)    # print data 1 page
-        self.ui.pushButton_3.clicked.connect(self.print_card2)    # print data 2 page
+        self.ui.pushButton.clicked.connect(self.action)  # print data - work
+        self.ui.pushButton_2.clicked.connect(self.print_card)  # print data 1 page
+        self.ui.pushButton_3.clicked.connect(self.print_card2)  # print data 2 page
 
     def action(self):
         self.ui.total_cycles.setText("")  # total_cycles
@@ -29,25 +30,29 @@ class MyWin(QtWidgets.QDialog):
         page = self.ui.page.toPlainText()
         counter = self.ui.counter.toPlainText()
 
-
         page1 = str(int(page) + 1)
         self.ui.next_page.setText(page1)
-
 
         total_cycles1 = str((int(cycles) + int(initial_cycles)) - int(f_cycles))
         self.ui.total_cycles1.setText(total_cycles1)
 
+        counter = int(counter)
+        if counter == 0:
+            total_cycles = str(int(cycles) + int(initial_cycles))
+            correction = str(0)
+        else:
+            total_cycles = str(counter)
+            correction = str(int(counter) - (int(cycles) + int(initial_cycles)))
 
-        total_cycles = str(int(cycles) + int(initial_cycles) + int(counter))
-        correction = str(int(counter) - int(total_cycles))
+        # total_cycles = str(int(cycles) + int(initial_cycles) + int(counter))
+        # correction = str(int(counter) - int(total_cycles))
 
         self.ui.total_cycles.setText(total_cycles)
         self.ui.correction.setText(correction)
 
-
         y_date = datetime.strftime(datetime.now(), "%Y")
 
-        #write data to file
+        # write data to file
         wb = load_workbook(filename='counter_ver.1.xlsx', read_only=True)
         ws = wb[y_date]
 
@@ -56,7 +61,7 @@ class MyWin(QtWidgets.QDialog):
             for cell in row:
                 if cell.value == mark:
                     x = cell.row  # find last mark row
-                    #print(x)
+                    # print(x)
                     break
 
         wrfile = load_workbook('counter_ver.1.xlsx')
@@ -80,7 +85,6 @@ class MyWin(QtWidgets.QDialog):
 
         wrfile.save('counter_ver.1.xlsx')
 
-
         # write data to template file, 1 page
         wrfile = load_workbook('Template_apl_cycles.xlsx')
 
@@ -95,7 +99,6 @@ class MyWin(QtWidgets.QDialog):
 
         wrfile.save('Template_apl_cycles.xlsx')
 
-
         # write data to template file, 2 page
         wrfile = load_workbook('Template_apl_cycles2.xlsx')
 
@@ -108,17 +111,16 @@ class MyWin(QtWidgets.QDialog):
 
         wrfile.save('Template_apl_cycles2.xlsx')
 
-
     # print 2 sheets in 2 file
     def print_card(self):
-        os.startfile("I:/python/code/cycles_apl/Template_apl_cycles.xlsx", "print")    # write correct address, file name
+        os.startfile("Template_apl_cycles.xlsx", "print")  # write correct address, file name
 
     def print_card2(self):
-        os.startfile("I:/python/code/cycles_apl/Template_apl_cycles2.xlsx", "print")  # write correct address, file name
+        os.startfile("Template_apl_cycles2.xlsx", "print")  # write correct address, file name
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     myapp = MyWin()
     myapp.show()
-    sys.exit(app.exec_())
+sys.exit(app.exec_())
