91f9bf1aca2ba44d80b3b4a5329916ac95609a05
Mytsa <viktor.mytsa@gmail.com>
2017-12-12 21:41:05 +0200

    add commit in code

--- win_test2.py
+++ win_test2.py
@@ -16,6 +16,7 @@ class MyWin(QtWidgets.QDialog):
         self.ui.pushButton_2.clicked.connect(self.print_card)  # print data 1 page
         self.ui.pushButton_3.clicked.connect(self.print_card2)  # print data 2 page
 
+
     def action(self):
         self.ui.total_cycles.setText("")  # total_cycles
         self.ui.next_page.setText("")  # next page
@@ -30,13 +31,14 @@ class MyWin(QtWidgets.QDialog):
         page = self.ui.page.toPlainText()
         counter = self.ui.counter.toPlainText()
 
+        # calcuclation
         page1 = str(int(page) + 1)
         self.ui.next_page.setText(page1)
 
         total_cycles1 = str((int(cycles) + int(initial_cycles)) - int(f_cycles))
         self.ui.total_cycles1.setText(total_cycles1)
 
-
+        # counter if loop
         counter = int(counter)
         if counter == 0:
             total_cycles = str(int(cycles) + int(initial_cycles))
@@ -45,34 +47,37 @@ class MyWin(QtWidgets.QDialog):
             total_cycles = str(counter)
             correction = str(int(counter) - (int(cycles) + int(initial_cycles)))
 
-
+        # write data after counter if loop
         self.ui.total_cycles.setText(total_cycles)
         self.ui.correction.setText(correction)
 
-        y_date = datetime.strftime(datetime.now(), "%Y")
+        # read/write data intro/to file
 
-        # write data to file
+        y_date = datetime.strftime(datetime.now(), "%Y")    # local variables for excel
+        date = datetime.strftime(datetime.now(), "%Y/%m/%d")    # local variables for excel
+
         wb = load_workbook(filename='counter_apl.xlsx', read_only=True)
         ws = wb[y_date]
 
-        mark = "**"  # word to search
+        mark = "**"  # word to search last raw in sheet
         for row in ws:
             for cell in row:
                 if cell.value == mark:
                     x = cell.row  # find last mark row, for data input place
                     break
 
+        # write data to counter file
         wrfile = load_workbook('counter_apl.xlsx')
 
-        date = datetime.strftime(datetime.now(), "%Y/%m/%d")
-
+        # index coordinate
         a = str('A') + str(x)
         b = str('B') + str(x)
         c = str('C') + str(x)
         d = str('D') + str(x)
         e = str('E') + str(x)
-        a1 = str('A') + str(x + 1)
+        a1 = str('A') + str(x + 1)    # for mark symbols
 
+        # write data
         sheet = wrfile.get_sheet_by_name(y_date)
         sheet[a] = int(applicator)
         sheet[b] = date
@@ -83,7 +88,7 @@ class MyWin(QtWidgets.QDialog):
 
         wrfile.save('counter_apl.xlsx')
 
-        # write data to template file, 1 page
+        # write/save data to template file, 1 page
         wrfile = load_workbook('Template_apl_cycles.xlsx')
 
         b = str('B') + str(8)
@@ -97,7 +102,7 @@ class MyWin(QtWidgets.QDialog):
 
         wrfile.save('Template_apl_cycles.xlsx')
 
-        # write data to template file, 2 page
+        # write/save data to template file, 2 page
         wrfile = load_workbook('Template_apl_cycles2.xlsx')
 
         d = str('D') + str(30)
