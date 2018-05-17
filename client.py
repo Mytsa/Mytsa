0c9e78f6908df1ed5085c42a9e5717c9d93c4301
Mytsa <viktor.mytsa@gmail.com>
2018-05-17 23:48:51 +0300

    work server-client, with run browser

--- client.py
+++ client.py
@@ -1,15 +1,50 @@
+# import socket
+# import json
+#
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# # Connect the socket to the port where the server is listening
+# server_address = ('localhost', 9999)
+# print('connecting to {} port {}'.format(*server_address))
+# sock.connect(server_address)
+#
+# try:
+#
+#     # Send data
+#     message = b'This is the message.  It will be repeated.'
+#     print('sending {!r}'.format(message))
+#     sock.sendall(message)
+#
+#     # Look for the response
+#     amount_received = 0
+#     amount_expected = len(message)
+#
+#     while amount_received < amount_expected:
+#         data = sock.recv(16)
+#         amount_received += len(data)
+#         print('received {!r}'.format(data))
+#
+# finally:
+#     print('closing socket')
+#     sock.close()
+
 #! /usr/bin/python3
 
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.200', 9999))
+s.connect(('localhost', 9999))
 
-s.sendall('Hello, world')
-data = s.recv(1024)
+def send(s):
+    s.send('e'.encode())
+    # data = ''
+    data = s.recv(1024).decode()
+    print('client', data)
+
+while 2:
+    r = input('enter')
+    send(s)
 s.close()
-print('Received', repr(data))
-
-
 
 
--- server.py
+++ server.py
@@ -1,12 +1,79 @@
+# import socket
+# import json
+#
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# # Bind the socket to the port
+# server_address = ('localhost', 9999)
+# print('starting up on {} port {}'.format(*server_address))
+# sock.bind(server_address)
+#
+# # Listen for incoming connections is 5 client
+# sock.listen(5)
+#
+# while True:
+#     # Wait for a connection
+#     print('waiting for a connection')
+#     connection, client_address = sock.accept()
+#     try:
+#         print('connection from', client_address)
+#
+#         # Receive the data in small chunks and retransmit it
+#         while True:
+#             data = connection.recv(16)
+#             print('received {!r}'.format(data))
+#             if data:
+#                 print('sending data back to the client')
+#                 connection.sendall(data)
+#             else:
+#                 print('no data from', client_address)
+#                 break
+#
+#     finally:
+#         # Clean up the connection
+#         connection.close()
+#
+
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 9999))
-s.listen(1)
-conn, address = s.accept()
+from threading import *
+from time import sleep
+from datetime import datetime
+
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(("localhost", 9999))
+
+class client(Thread):
+    def __init__(self, socket, address, tst):
+        Thread.__init__(self)
+        self.sock = socket
+        self.adrs = address
+        self.tst = tst
+        self.start()
+
+
+    def run(self):
+        while 1:
+            print('Client from server sent:', self.sock.recv(1024).decode())
+            a = str(self.tst)
+            self.sock.send(a.encode())
+
+
+serversocket.listen(5)
+print('server started and listening')
 while 1:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
-conn.close()
+    for i in range(0, 10):
+        tst = datetime.timestamp(datetime.now())
+        clientsocket, address = serversocket.accept()
+        client(clientsocket, address, tst)
+        print(i)
+        sleep(1)
+        i += i
+    # sleep(10)
+    # print("finish server")
+    # serversocket.close()
+
+
+
 
